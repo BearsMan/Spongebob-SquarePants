5e64492d670e74e21a84fc71576cb52019209d98
Darius Ronkowitz <dronkowitz@yahoo.com>
2024-06-25 06:34:53 -0400

    `Updated SpongeBob Game Code`
    Added JellyFish.cs and SpawnPoint.cs

---
+++ SpawnPoint.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
