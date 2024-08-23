dcd96b4618809203cfe989fb1d2d7446994eae91
Darius Ronkowitz <dronkowitz@yahoo.com>
2024-08-22 20:09:28 -0400

    `Updated DialogTrigger.cs`
    Fixed an issue with dialog not being set correctly during gameplay.
    Fixed minor code bugs.

--- DialogTrigger.cs
+++ DialogTrigger.cs
@@ -1,17 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class DialogTrigger : MonoBehaviour
 {
     public GameObject dialogPanel;
-    public Collider ColliderNPC;
-    public TextMeshPro spongeBobText;
-    public TextMeshPro squidwardText;
-    public TextMeshPro patrickStarText;
+    public GameObject currentNPC;
+    public TextMeshProUGUI spongeBobText;
+    public TextMeshProUGUI squidwardText;
+    public TextMeshProUGUI patrickStarText; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         dialogPanel.SetActive(false);
+        spongeBobText.gameObject.SetActive(false);
+        squidwardText.gameObject.SetActive(false);
+        patrickStarText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,14 +24,22 @@ public class DialogTrigger : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            dialogPanel.SetActive(false);
+            dialogPanel.SetActive(true);
+            if (currentNPC.CompareTag("SpongeBob SquarePants"))
+            {
+                spongeBobText.gameObject.SetActive(true);
+            }
+            if (currentNPC.CompareTag("Squidward"))
+            {
+                squidwardText.gameObject.SetActive(true);
+            }
         }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("SpongeBob SquarePants"))
         {
-            spongeBobText.gameObject.SetActive(false);
+            spongeBobText.gameObject.SetActive(true);
         }
     }
 }
