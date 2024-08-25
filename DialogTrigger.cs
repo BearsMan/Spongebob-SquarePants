dd7f26049d92dbd6d0c0af00674d149fc9e69f4e
Darius Ronkowitz <dronkowitz@yahoo.com>
2024-08-25 17:18:09 -0400

    `Updated DialogTrigger.cs`
    Fixed a bug with Sandy's model import from Blender.
    Added extra code to compare tags and dialog triggers with comments.

--- DialogTrigger.cs
+++ DialogTrigger.cs
@@ -24,22 +24,38 @@ public class DialogTrigger : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            // Set dialog panel to the correct NPC model.
+
             dialogPanel.SetActive(true);
             if (currentNPC.CompareTag("SpongeBob SquarePants"))
             {
-                spongeBobText.gameObject.SetActive(true);
+                spongeBobText.gameObject.SetActive(true); // Set to false if you do not want the text to appear.
             }
             if (currentNPC.CompareTag("Squidward"))
             {
-                squidwardText.gameObject.SetActive(true);
+                squidwardText.gameObject.SetActive(true); // Set to false if you do not want the text to appear.
+            }
+            if (currentNPC.CompareTag("Patrick Star"))
+            {
+                patrickStarText.gameObject.SetActive(true); // Set to false if you do not want the text to appear.
             }
         }
     }
     private void OnTriggerEnter(Collider other)
     {
+        // Set each compare tag to the correct model dialog.
+
         if (other.CompareTag("SpongeBob SquarePants"))
         {
             spongeBobText.gameObject.SetActive(true);
+        }
+        if (other.CompareTag("Squidward Tentacles"))
+        {
+            squidwardText.gameObject.SetActive(true);
+        }
+        if (other.CompareTag("Patrick Star"))
+        {
+            patrickStarText.gameObject.SetActive(true);
         }
     }
 }
